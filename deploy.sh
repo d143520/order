b8abd40ddd86658effedcccd9a7a8c923d9c5a00
baiwei <baiwei@hitachi-ebworx.com>
2022-07-31 22:02:09 +0800

    modfied deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,12 +20,13 @@ killTomcat()
       kill -9 $pid
     fi
 }
+echo "======Starting the deploy.sh"
 cd $PROJ_PATH/order
 mvn clean install
-
+echo "done the maven install!!!"
 # 停tomcat
 killTomcat
-
+echo "======Stoped tomcat"
 # 删除原有工程
 rm -rf $TOMCAT_APP_PATH/webapps/ROOT
 rm -f $TOMCAT_APP_PATH/webapps/ROOT.war
@@ -36,10 +37,10 @@ cp $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
 
 cd $TOMCAT_APP_PATH/webapps/
 mv order.war ROOT.war
-
+echo "======Starting tomcat...."
 # 启动Tomcat
 cd $TOMCAT_APP_PATH/
 sh bin/startup.sh
-
+echo "======Started tomcat"
 
 
